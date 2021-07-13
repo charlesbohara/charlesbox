2a15fe79b920e8cb99c8941f3ed74a60cbe4cb9f
charlesbohara <87374180+charlesbohara@users.noreply.github.com>
2021-07-13 18:51:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var charles
+function setup() {
+  createCanvas(400,400);
+  charles = createSprite(200,200,30,30);
+  charles.shapeColor = "red"
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
